28496c0101f7343eb3820badddd85b6500116d1f
rivka yalo <rivkayalo73@gmail.com>
2022-09-10 13:47:51 +0300

    gh repo clone Ririvka/homework---hacker-U

--- homework4/index.js
+++ homework4/index.js
@@ -1,17 +1,37 @@
+// function miniCalculator() {
+//   let firstnam = +document.getElementById("firstnam").value;
+//   let secondnam = +document.getElementById("secondnam").value;
+//   let finalsolution = document.getElementById("finalsolution").value;
+//   let finalnamber;
+
+//   if (finalsolution == "opA") {
+//     finalnamber = firstnam + secondnam;
+//   } else if (finalsolution == "opB") {
+//     finalnamber = firstnam - secondnam;
+//   } else if (finalsolution == "opC") {
+//     finalnamber = firstnam * secondnam;
+//   } else if (finalsolution == "opD") {
+//     finalnamber = firstnam / secondnam;
+//   }
+//   document.getElementById(`solution`).innerText = finalnamber;
+// }
+
 function miniCalculator() {
-  let firstnam = +document.getElementById("firstnam").value;
-  let secondnam = +document.getElementById("secondnam").value;
-  let finalsolution = document.getElementById("finalsolution").value;
+  let firstnam = +prompt("firstnam");
+  let option = prompt("+, -, *, /");
+  let secondnam = +prompt("secondnam");
+  // let finalsolution = prompt("finalsolution");
   let finalnamber;
 
-  if (finalsolution == "opA") {
+  if (option == "+") {
     finalnamber = firstnam + secondnam;
-  } else if (finalsolution == "opB") {
+  } else if (option == "-") {
     finalnamber = firstnam - secondnam;
-  } else if (finalsolution == "opC") {
+  } else if (option == "*") {
     finalnamber = firstnam * secondnam;
-  } else if (finalsolution == "opD") {
+  } else if (option == "/") {
     finalnamber = firstnam / secondnam;
   }
-  document.getElementById(`solution`).innerText = finalnamber;
+  alert(finalnamber);
+  // let(`solution`).innerText = finalnamber;
 }
